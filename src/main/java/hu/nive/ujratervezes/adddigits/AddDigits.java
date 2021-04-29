e211f697c7434344c4b289187d3f26af772029d9
GDandris <andris.gede@gmail.com>
2021-04-29 10:16:12 +0200

    Tasks done

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,16 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
-    //todo: implement your solution here
+
+    public int addDigits(String input){
+        if(input == null || input.equals("")){
+            return -1;
+        }
+        int sum = 0;
+        input = input.replaceAll("[^\\d.]", "");
+        for(int i = 0; i<input.length();i++){
+            sum += Character.getNumericValue(input.charAt(i));
+        }
+        return sum;
+    }
 }
